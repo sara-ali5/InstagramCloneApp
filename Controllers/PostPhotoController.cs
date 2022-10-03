5a5515740f19e7f33f951f49f8abafb24c9d583c
sara-ali5 <66339549+sara-ali5@users.noreply.github.com>
2022-10-03 19:33:27 +0200

    Add files via upload

---
+++ Controllers/PostPhotoController.cs
@@ -0,0 +1,31 @@
+﻿using insta.Context;
+using insta.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace insta.Controllers
+{
+    public class PostPhotoController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: PostPhoto
+        public ActionResult PostPhoto()
+        {
+            if (Session["Userid"] == "0" && Session["Userid"] == null)
+            {
+                return RedirectToAction("User", "LogIn");
+            }
+
+            User userID = db.User.Find(Convert.ToInt32(Session["Userid"]));
+            return View(userID);
+        }
+
+
+        
+    }
+}
